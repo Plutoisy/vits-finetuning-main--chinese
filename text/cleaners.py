7b67e3db96e83287f58fbc8b016dd20a4559f14a
Plutoisy <89139849+YBY2002@users.noreply.github.com>
2023-02-18 16:48:31 +0800

    first commit

---
+++ text/cleaners.py
@@ -0,0 +1,14 @@
+import re
+from text.mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo, chinese_to_romaji, chinese_to_lazy_ipa, chinese_to_ipa, chinese_to_ipa2
+
+def chinese_cleaners(text):
+    '''Pipeline for Chinese text'''
+    text = number_to_chinese(text)
+    text = chinese_to_bopomofo(text)
+    text = latin_to_bopomofo(text)
+    text = re.sub(r'([ˉˊˇˋ˙])$', r'\1。', text)
+    return text
+
+
+
+
